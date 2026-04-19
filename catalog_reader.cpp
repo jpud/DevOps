d32e67746796c3f9767d1f1f7aedbf06a52630c2
jpud <31748987+jpud@users.noreply.github.com>
2026-04-19 17:40:49 -0500

    Add files via upload

---
+++ catalog_reader.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <xmlfile>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    std::string line;
+    while (std::getline(file, line)) {
+        size_t start = line.find("<title>");
+        size_t end = line.find("</title>");
+        if (start != std::string::npos && end != std::string::npos) {
+            start += 7;
+            std::cout << line.substr(start, end - start) << std::endl;
+        }
+    }
+    return 0;
+}
