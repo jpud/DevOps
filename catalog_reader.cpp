ee38737290876d8b7a41af0472d965270165d0bf
jpud <31748987+jpud@users.noreply.github.com>
2026-05-05 14:56:51 -0500

    Pipeline test

--- catalog_reader.cpp
+++ catalog_reader.cpp
@@ -18,6 +18,6 @@ int main(int argc, char* argv[]) {
             std::cout << line.substr(start, end - start) << std::endl;
         }
     }
-    
+    std::cout<<"Pipelinetest"<<std::endl;
     return 0;
 }
